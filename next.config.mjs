bcedf6726aa4d0b4f6a73fab310c369aa139327d
Director M.O.G. <director.mog@gmail.com>
2026-04-10 14:01:52 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+  // This helps prevent build errors related to ESLint during the Vercel deployment
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
