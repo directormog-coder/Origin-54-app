cfe16f471a0acda2b209de2cca1b73701c49596b
Director M.O.G. <director.mog@gmail.com>
2026-06-09 08:19:03 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
-  ...nextVitals,
-  {
-    rules: {
-      'react-hooks/rules-of-hooks': 'off', // Bypasses circular reference crash
-    }
-  }
+  ...compat.extends("next/core-web-vitals.js"),
 ];
 
 export default eslintConfig;
